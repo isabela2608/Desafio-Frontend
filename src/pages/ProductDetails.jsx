c5469b05747cbb029f663903167ad2d6392e8e6d
Isabela <isabelamaria2608@gmail.com>
2026-03-28 22:30:43 -0300

    feat: adiciona contador de quantidade no ProductDetails

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -6,6 +6,7 @@ import loading from "./../assets/loading.svg"
 function ProductDetails() {
   const { id } = useParams()
   const [product, setProduct] = useState(null)
+  const [quantity, setQuantity] = useState(1)
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -17,7 +18,16 @@ function ProductDetails() {
     fetchProduct()
   }, [id])
 
-  // LOADING
+  const increase = () => {
+    setQuantity(quantity + 1)
+  }
+
+  const decrease = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1)
+    }
+  }
+
   if (!product) {
     return (
       <div className={styles.loading_container}>
@@ -26,22 +36,25 @@ function ProductDetails() {
     )
   }
 
+  const totalPrice = product.price * quantity
+
   return (
     <section className={styles.container}>
 
       <div className={styles.image_container}>
-  
-      <Link to="/products" className={styles.back_button}>
-       ← Voltar
-      </Link>
+        <Link to="/products" className={styles.back_button}>
+          ← Voltar
+        </Link>
 
-      <img src={product.image} alt={product.title} />
+        <img src={product.image} alt={product.title} />
       </div>
 
       <div className={styles.info_container}>
         <h1>{product.title}</h1>
 
-        <p className={styles.price}>R$ {product.price}</p>
+        <p className={styles.price}>
+          R$ {product.price}
+        </p>
 
         <p className={styles.category}>
           Categoria: {product.category}
@@ -51,9 +64,24 @@ function ProductDetails() {
           {product.description}
         </p>
 
-        <button className={styles.button}>
-          Adicionar ao carrinho
-        </button>
+        {/* 🔥 LINHA COM CONTADOR + BOTÃO */}
+        <div className={styles.actions}>
+
+          <div className={styles.counter}>
+            <button onClick={decrease}>-</button>
+            <span>{quantity}</span>
+            <button onClick={increase}>+</button>
+          </div>
+
+          <button className={styles.btnCart}>
+            <span>Adicionar</span>
+            <span className={styles.bntprice}>
+              R$ {totalPrice.toFixed(2)}
+            </span>
+          </button>
+
+        </div>
+
       </div>
 
     </section>
