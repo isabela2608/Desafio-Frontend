62bc5390d5eabdc37684eb2d57c071df7d01bec6
Isabela <isabelamaria2608@gmail.com>
2026-04-19 20:48:59 -0300

    feat: implementar barra de busca na navbar
    - Busca dinâmica na página de produtos
    
    - Integração com estado global de pesquisa
    
    - Condicional para exibir busca apenas em /products

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,18 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom"; 
 import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
-import { FaShoppingCart } from "react-icons/fa"
+import { FaShoppingCart, FaSearch } from "react-icons/fa";
 
 import Container from "./Container";
 import styles from "./Navbar.module.css";
 
 import logo from "../../assets/logo.jpeg";
 
-function Navbar() {
+function Navbar({ search, setSearch }) {
 
   const { cart } = useContext(CartContext)
+  const location = useLocation()
 
-  // Soma dos itens
   const totalItems = cart.reduce((sum, item) => {
     return sum + item.quantity
   }, 0)
@@ -20,38 +20,50 @@ function Navbar() {
   return (
     <nav className={styles.navbar}>
       <Container>
+        <div className={styles.nav_content}>
 
-        <Link to="/">
-          <img src={logo} alt="Logo" className={styles.logo} />
-        </Link>
+          <Link to="/">
+            <img src={logo} alt="Logo" className={styles.logo} />
+          </Link>
 
-        <ul className={styles.list}>
-          <li className={styles.item}>
-            <Link to="/products">Produtos</Link>
-          </li>
+          <div className={styles.right_side}>
 
-          <li className={styles.item}>
-  <Link to="/cart" className={styles.cartLink}>
-    
-    <div className={styles.cartContainer}>
-      <FaShoppingCart className={styles.cartIcon} />
+            {location.pathname === "/products" && (
+              <div className={styles.search_box}>
+                <FaSearch className={styles.search_icon} />
+                <input
+                  type="text"
+                  placeholder="Buscar produtos ou categorias..."
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                  className={styles.search_input}
+                />
+              </div>
+            )}
 
-      {totalItems > 0 && (
-        <span key={totalItems} className={styles.badge}>
-          {totalItems}
-        </span>
-      )}
-    </div>
+            <ul className={styles.list}>
+              <li className={styles.item}>
+                <Link to="/products">Produtos</Link>
+              </li>
 
-  </Link>
-</li>
+              <li className={styles.item}>
+                <Link to="/cart">
+                  <div className={styles.cartContainer}>
+                    <FaShoppingCart className={styles.cartIcon} />
 
-          <li className={styles.item}>
-            <Link to="/login">Login</Link>
-          </li>
-          
-        </ul>
+                    {totalItems > 0 && (
+                      <span className={styles.badge}>
+                        {totalItems}
+                      </span>
+                    )}
+                  </div>
+                </Link>
+              </li>
+            </ul>
 
+          </div>
+
+        </div>
       </Container>
     </nav>
   );
