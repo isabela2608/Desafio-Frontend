504f6e8a14479da65f4e54eec48fa19a6f081fb8
Isabela <isabelamaria2608@gmail.com>
2026-04-19 20:44:10 -0300

    feat: implementar busca global entre navbar e produtos

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,43 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useState } from "react"
 
 import Home from './pages/Home'
 import Navbar from './components/layout/Navbar'
 import Products from './pages/Products'
 import ProductDetails from './pages/ProductDetails'
 import Cart from './pages/Cart'
-import Login from './pages/Login'
+
 import { CartProvider } from "./context/CartContext"
 
 function App() {
+
+  const [search, setSearch] = useState("")
+
   return (
-      <CartProvider>
-    <Router>
+    <CartProvider>
+      <Router>
 
-      <Navbar />
+        <Navbar search={search} setSearch={setSearch} />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
 
-    </Router>
-      </CartProvider>
+          <Route 
+            path="/products" 
+            element={
+              <Products 
+                search={search} 
+                setSearch={setSearch} 
+              />
+            } 
+          />
+
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+
+      </Router>
+    </CartProvider>
   );
 }
 
